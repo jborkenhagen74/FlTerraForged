44d7271e53c65863b105501c63a2f3e5dbe6caec
jborkenhagen <jan.borkenhagen@yahoo.de>
2026-09-06 11:31:15 +0200

    R57: update engine API version regression test

--- engine-api/src/test/java/dev/foucaultleon/flterraforged/engine/api/EngineApiVersionTest.java
+++ engine-api/src/test/java/dev/foucaultleon/flterraforged/engine/api/EngineApiVersionTest.java
@@ -10,6 +10,6 @@ class EngineApiVersionTest {
     @Test
     void comparesSemanticComponents() {
         assertTrue(new EngineApiVersion(0, 2, 0).compareTo(new EngineApiVersion(0, 1, 9)) > 0);
-        assertEquals("0.1.0", EngineApiVersion.CURRENT.toString());
+        assertEquals("0.2.0", EngineApiVersion.CURRENT.toString());
     }
 }
